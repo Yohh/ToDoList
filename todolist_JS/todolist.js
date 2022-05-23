a88093443465ab1acf944337e393cf081d83a96a
Yoh <yoh@debian-BULLSEYE-live-builder-AMD64>
2022-05-23 15:23:22 +0200

    add alert for empty task

--- todolist_JS/todolist.js
+++ todolist_JS/todolist.js
@@ -4,22 +4,26 @@ const toDo = document.querySelector("#toDo");
 
 form.onsubmit = function (event) {
   event.preventDefault();
-  const newLine = document.createElement("form");
-  newLine.classList.add("newLine");
-  newLine.style.display = "flex";
-  newLine.style.justifyContent = "space-between";
-  newLine.style.alignItems = "center";
-  toDo.appendChild(newLine);
-  const newTask = document.createElement("li");
-  newTask.innerHTML = taskInput.value;
-  newLine.appendChild(newTask);
-  const button = document.createElement("input");
-  button.type = "submit";
-  button.value = "";
-  button.classList.add("remove");
-  newLine.appendChild(button);
-  newLine.onsubmit = function () {
-    newLine.remove();
-  };
-  taskInput.value = "";
+  if (taskInput.value == "") {
+    alert("no task found");
+  } else {
+    const newLine = document.createElement("form");
+    newLine.classList.add("newLine");
+    newLine.style.display = "flex";
+    newLine.style.justifyContent = "space-between";
+    newLine.style.alignItems = "center";
+    toDo.appendChild(newLine);
+    const newTask = document.createElement("li");
+    newTask.innerHTML = taskInput.value;
+    newLine.appendChild(newTask);
+    const button = document.createElement("input");
+    button.type = "submit";
+    button.value = "";
+    button.classList.add("remove");
+    newLine.appendChild(button);
+    newLine.onsubmit = function () {
+      newLine.remove();
+    };
+    taskInput.value = "";
+  }
 };
